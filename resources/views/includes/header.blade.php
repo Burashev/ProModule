55d1cc7335c9a3030dcded3dac80cb80adef2652
Burashev Shamil <shburashev@ya.ru>
2023-02-23 23:46:19 +0300

    feat: add basic blade layouts

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,23 @@
+<header class="header">
+    <div class="container">
+        <div class="header__body">
+            <div class="logo">
+                <h2>{{env('APP_NAME')}}</h2>
+            </div>
+            <ul class="header__menu">
+                <li>
+                    <a href="#" class="active">Menu 1</a>
+                </li>
+                <li>
+                    <a href="#">Menu 2</a>
+                </li>
+                <li>
+                    <a href="#">Menu 3</a>
+                </li>
+            </ul>
+            <div class="header__auth-menu">
+                <a href="#" class="btn">Вступить</a>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <title>@yield('title', '') | {{env('APP_NAME')}}</title>
+</head>
+<body>
+@include('includes.header')
+<section class="auth-section">
+    <div class="container">
+        <div class="auth-section__body">
+            @yield('content')
+        </div>
+    </div>
+</section>
+</body>
+</html>

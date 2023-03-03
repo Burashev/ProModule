74543092ebf96f258fec6cffe50d4077d8374074
Burashev Shamil <shburashev@ya.ru>
2023-03-03 20:45:49 +0300

    feat(File): add link accessor to File model

--- src/Domains/File/Models/File.php
+++ src/Domains/File/Models/File.php
@@ -2,11 +2,12 @@
 
 namespace Domains\File\Models;
 
+use Domains\File\Support\FileManager;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
 
 /**
  * @method static Builder|File query()
@@ -25,5 +26,11 @@ class File extends Model
 
     public function fileLinks(): HasMany {
         return $this->hasMany(FileLink::class);
+    }
+
+    public function link(): Attribute {
+        return Attribute::make(
+            get: fn() => (new FileManager())->createFileLink($this)
+        );
     }
 }
